a288a8fda47cb778c2f0fecef1655a5cf61817fa
Barrett1111 <rushikeshranaware22sep@gmail.com>
2025-01-13 19:57:00 +0530

    Static Block

---
+++ StaticBlock.java
@@ -0,0 +1,23 @@
+class Test{
+
+    static{
+        System.out.println("Hello from Static first");
+    }
+
+    static{
+        System.out.println("Hello from Static Second");
+    }
+
+    static{
+        System.out.println("Hello from Static Third");
+    }
+}
+
+
+public class StaticBlock {
+    public static void main(String[] args) {
+        Test t = new Test(); // For executing static just use class i.e making object of class loads the class.
+                             // thus, without using object, Static block gets called.
+
+    }
+}
