b8aa377cf1f5eda9a6b7c1d803c25941eceb7d5c
Barrett1111 <rushikeshranaware22sep@gmail.com>
2025-01-16 19:51:47 +0530

    Exception

---
+++ ExceptionHandling.java
@@ -0,0 +1,24 @@
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int A[]={1,2,3,4,5};
+        try{
+           
+            int a=10;
+            int b=10;
+            int c=b/a;
+
+            try{                                    /////////////////////
+                int d;
+                System.out.println(A[10]);
+            }                                                   //NESTED*************
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e);             //////////////////////
+            }
+
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+    }
+}
